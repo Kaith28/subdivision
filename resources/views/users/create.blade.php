1c48b2cd230f2461cadc6301393e6d7feb80b181
HartCardano <habibicardano@gmail.com>
2023-11-11 18:16:02 +0800

    box create user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex flex-col items-center shadow-lg rounded-md p-4">
             <form method="POST" action="">
                 @csrf
 
