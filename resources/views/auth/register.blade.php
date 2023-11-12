01d1aefbbe5096b3127bd8e6e892226e8d086502
HartCardano <habibicardano@gmail.com>
2023-11-12 08:12:12 +0800

    register add photo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,6 +45,13 @@
             <x-text-input id="company_name" class="block mt-1 w-full" type="text" name="company_name" :value="old('company_name')" required autofocus autocomplete="company_name" />
             <x-input-error :messages="$errors->get('company_name')" class="mt-2" />
         </div>
+         <!-- Upload photo -->
+         <div class="mt-4">
+            <x-input-label for="photo" :value="__('Add Photo')" />
+            <x-text-input id="photo" class="block mt-1 w-full" type="file" name="photo"
+                :value="old('photo')" required autofocus autocomplete="photo" />
+            <x-input-error :messages="$errors->get('photo')" class="mt-2" />
+        </div> 
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
