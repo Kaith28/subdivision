b8e0910766a3c6be2edfeff5911b4dd3196589d1
Kaith28 <kittykaith4@gmail.com>
2023-11-16 00:23:05 +0800

    update show info in guest as a guard

--- resources/views/guest/guest.blade.php
+++ resources/views/guest/guest.blade.php
@@ -75,8 +75,8 @@
                                                 </svg></a>
                                         @endif --}}
 
-                                        @if (Auth::user()->role == 'admin' || Auth::user()->role == 'owner')
-                                            <form action={{ route('resident.destroy', $user->id) }} method="POST">
+                                        {{-- @if (Auth::user()->role == 'admin' || Auth::user()->role == 'owner')
+                                            <form action={{ route('guest.destroy', $guest->id) }} method="POST">
                                                 @csrf
                                                 <button class="px-2 py-2 hover:bg-gray-200 rounded-md "><svg
                                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -87,7 +87,7 @@
                                                             d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
                                                     </svg></button>
                                             </form>
-                                        @endif
+                                        @endif --}}
                                     </div>
                                 </td>
                             </tr>
