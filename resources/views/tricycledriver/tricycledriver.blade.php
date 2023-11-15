8f63648ec9697192bc10edd1c819c2e595a54b25
Kaith28 <kittykaith4@gmail.com>
2023-11-15 17:53:56 +0800

    remove some titles

--- resources/views/tricycledriver/tricycledriver.blade.php
+++ resources/views/tricycledriver/tricycledriver.blade.php
@@ -8,12 +8,12 @@
         </div>
     @endif
 
-    <x-slot name="header">
+    {{--  <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Tricycle Driver') }}
         </h2>
     </x-slot>
-
+ --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-between items-center">
