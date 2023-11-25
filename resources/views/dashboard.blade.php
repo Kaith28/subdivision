c45f480a4d96d0a94cceeca9a6b2d64856c07332
Kaith28 <kittykaith4@gmail.com>
2023-11-25 15:27:22 +0800

    fix dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,17 +12,6 @@
                 </div>
                 <div class="flex flex-col items-center shadow-lg rounded-md p-4">
                     <div class="h-20 flex items-center">
-                        <h2 class="text-4xl">{{ $totalInGuests }}</h2>
-                    </div>
-                    <p class="text-xl ">
-                        TOTAL Guests
-                    </p>
-                    <p class="text-sm">
-                        Total Visitors: {{ $totalOutGuests }}
-                    </p>
-                </div>
-                <div class="flex flex-col items-center shadow-lg rounded-md p-4">
-                    <div class="h-20 flex items-center">
                         <h2 class="text-4xl">{{ $totalIn }}</h2>
                     </div>
                     <p class="text-xl ">
@@ -35,6 +24,17 @@
                     </div>
                     <p class="text-xl">
                         TOTAL OUT
+                    </p>
+                </div>
+                <div class="flex flex-col items-center shadow-lg rounded-md p-4">
+                    <div class="h-20 flex items-center">
+                        <h2 class="text-4xl">{{ $totalInGuests }}</h2>
+                    </div>
+                    <p class="text-xl ">
+                        TOTAL Guests
+                    </p>
+                    <p class="text-sm">
+                        Total Visitors: {{ $totalOutGuests }}
                     </p>
                 </div>
             </div>
@@ -54,84 +54,72 @@
     </div>
 
     {{-- Bar Chart Here! --}}
-    <?php
- 
-    $dataPoints1 = array(
-        array("label"=> "2010", "y"=> 36.12),
-        array("label"=> "2011", "y"=> 34.87),
-        array("label"=> "2012", "y"=> 40.30),
-        array("label"=> "2013", "y"=> 35.30),
-        array("label"=> "2014", "y"=> 39.50),
-        array("label"=> "2015", "y"=> 50.82),
-        array("label"=> "2016", "y"=> 74.70)
-    );
-    $dataPoints2 = array(
-        array("label"=> "2010", "y"=> 64.61),
-        array("label"=> "2011", "y"=> 70.55),
-        array("label"=> "2012", "y"=> 72.50),
-        array("label"=> "2013", "y"=> 81.30),
-        array("label"=> "2014", "y"=> 63.60),
-        array("label"=> "2015", "y"=> 69.38),
-        array("label"=> "2016", "y"=> 98.70)
-    );
+    <div class="flex ">
+        <?php
         
-    ?>
-    <!DOCTYPE HTML>
-    <html>
-    <head>  
-    <script>
-    window.onload = function () {
-     
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Total of IN and OUT"
-        },
-        axisY:{
-            includeZero: true
-        },
-        legend:{
-            cursor: "pointer",
-            verticalAlign: "center",
-            horizontalAlign: "right",
-            itemclick: toggleDataSeries
-        },
-        data: [{
-            type: "column",
-            name: "IN",
-            indexLabel: "{y}",
-            yValueFormatString: "$#0.##",
-            showInLegend: true,
-            dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
-        },{
-            type: "column",
-            name: "OUT",
-            indexLabel: "{y}",
-            yValueFormatString: "$#0.##",
-            showInLegend: true,
-            dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
-        }]
-    });
-    chart.render();
-     
-    function toggleDataSeries(e){
-        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        }
-        else{
-            e.dataSeries.visible = true;
-        }
-        chart.render();
-    }
-     
-    }
-    </script>
-    </head>
-    <body>
-    <div id="chartContainer" style="height: 370px; width: 100%; mb: 20;"></div>
-    <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
-    </body>
-    </html>          
+        $dataPoints1 = [['label' => '2010', 'y' => 36.12], ['label' => '2011', 'y' => 34.87], ['label' => '2012', 'y' => 40.3], ['label' => '2013', 'y' => 35.3], ['label' => '2014', 'y' => 39.5], ['label' => '2015', 'y' => 50.82], ['label' => '2016', 'y' => 74.7]];
+        $dataPoints2 = [['label' => '2010', 'y' => 64.61], ['label' => '2011', 'y' => 70.55], ['label' => '2012', 'y' => 72.5], ['label' => '2013', 'y' => 81.3], ['label' => '2014', 'y' => 63.6], ['label' => '2015', 'y' => 69.38], ['label' => '2016', 'y' => 98.7]];
+        
+        ?>
+        <!DOCTYPE HTML>
+        <html>
+
+        <head>
+            <script>
+                window.onload = function() {
+
+                    var chart = new CanvasJS.Chart("chartContainer", {
+                        animationEnabled: true,
+                        theme: "light2",
+                        title: {
+                            text: "Total of IN and OUT"
+                        },
+                        axisY: {
+                            includeZero: true
+                        },
+                        legend: {
+                            cursor: "pointer",
+                            verticalAlign: "center",
+                            horizontalAlign: "right",
+                            itemclick: toggleDataSeries
+                        },
+                        data: [{
+                            type: "column",
+                            name: "IN",
+                            indexLabel: "{y}",
+                            yValueFormatString: "$#0.##",
+                            showInLegend: true,
+                            dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+                        }, {
+                            type: "column",
+                            name: "OUT",
+                            indexLabel: "{y}",
+                            yValueFormatString: "$#0.##",
+                            showInLegend: true,
+                            dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+                        }]
+                    });
+                    chart.render();
+
+                    function toggleDataSeries(e) {
+                        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                            e.dataSeries.visible = false;
+                        } else {
+                            e.dataSeries.visible = true;
+                        }
+                        chart.render();
+                    }
+
+                }
+            </script>
+        </head>
+
+        <body>
+            <div id="chartContainer" style="height: 370px; width: 100%; mb: 20;"></div>
+            <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+        </body>
+    </div>
+
+
 
 </x-app-layout>
