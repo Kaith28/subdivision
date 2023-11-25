6a16ec3661a938a98e8be9f2c4ddf359a88997a7
Kaith28 <kittykaith4@gmail.com>
2023-11-25 13:32:17 +0800

    update dashboard count tricycle driver

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,15 +13,17 @@ class DashboardController extends Controller
     {
         $totalResidents = User::where('role', 'resident')->count();
         $totalInResidents = User::where('role', 'resident')->where('status', 'in')->count();
+        $totalInTricycleDrivers = User::where('role', 'driver')->where('status', 'in')->count();
         $totalInGuests = Guest::where('out', null)->count();
 
         $totalOutResidents = User::where('role', 'resident')->where('status', 'out')->count();
+        $totalOutTricycleDrivers = User::where('role', 'driver')->where('status', 'out')->count();
         $totalOutGuests = Guest::where('out', '!=', null)->count();
 
         return view('dashboard', [
             'totalResidents' => $totalResidents,
-            'totalIn' => $totalInResidents + $totalInGuests,
-            'totalOut' => $totalOutResidents + $totalOutGuests
+            'totalIn' => $totalInResidents + $totalInGuests + $totalInTricycleDrivers,
+            'totalOut' => $totalOutResidents + $totalOutGuests + $totalOutTricycleDrivers
         ]);
     }
 }
