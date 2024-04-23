64f800b80a07b5622781e671f96a245f8d13e294
Kaith28 <kittykaith4@gmail.com>
2024-04-23 11:14:21 +0800

    fix validation in events create

--- resources/views/events/events.blade.php
+++ resources/views/events/events.blade.php
@@ -4,33 +4,37 @@
             <div id="calendar"></div>
         </div>
     </div>
-    <div id="modal" class="hidden absolute top-0 z-50 w-full h-full bg-black/50 flex justify-center items-center">
-        <form action={{ route('events.store') }} method="POST">
-            @csrf
-            <div class="bg-white w-96 p-5 rounded-md flex flex-col gap-4">
-                <h2 class="font-bold">Add Event</h2>
-                <div class="w-full flex flex-col gap-2">
-                    <input type="text" name="organizer" placeholder="Organizer" class="w-full rounded-md">
-                    <input type="text" name="address" placeholder="Address" class="w-full rounded-md">
-                    <input type="text" name="contact_no" placeholder="Contact no." class="w-full rounded-md">
-                    <input type="text" name="event_location" placeholder="Event location" class="w-full rounded-md">
-                    <input type="text" name="event_purpose" placeholder="Event purpose" class="w-full rounded-md">
-                    <input type="number" name="estimated_attendees" placeholder="Est attendees"
-                        class="w-full rounded-md">
-                    <input type="text" name="time" placeholder="Time" class="w-full rounded-md">
+    @if (Auth::user()->role == 'admin' || Auth::user()->role == 'owner')
+        <div id="modal" class="hidden absolute top-0 z-50 w-full h-full bg-black/50 flex justify-center items-center">
+            <form action={{ route('events.store') }} method="POST">
+                @csrf
+                <div class="bg-white w-96 p-5 rounded-md flex flex-col gap-4">
+                    <h2 class="font-bold">Add Event</h2>
+                    <div class="w-full flex flex-col gap-2">
+                        <input type="text" name="organizer" placeholder="Organizer" class="w-full rounded-md">
+                        <input type="text" name="address" placeholder="Address" class="w-full rounded-md">
+                        <input type="text" name="contact_no" placeholder="Contact no." class="w-full rounded-md">
+                        <input type="text" name="event_location" placeholder="Event location"
+                            class="w-full rounded-md">
+                        <input type="text" name="event_purpose" placeholder="Event purpose"
+                            class="w-full rounded-md">
+                        <input type="number" name="estimated_attendees" placeholder="Est attendees"
+                            class="w-full rounded-md">
+                        <input type="text" name="time" placeholder="Time" class="w-full rounded-md">
 
-                    <input id="date" type="date" name="date" placeholder="Date" class="w-full rounded-md"
-                        readonly>
-                    <div class=" flex  flex-col justify-center w-full gap-2">
-                        <button class="px-4 py-2  gap-2 bg-orange-200 hover:bg-orange-300 rounded-md"
-                            onclick="addEvent()">Add</button>
-                        <button class="px-4 py-2  gap-2 bg-red-400 hover:bg-red-500 rounded-md"
-                            onclick="cancel()">Cancel</button>
+                        <input id="date" type="date" name="date" placeholder="Date" class="w-full rounded-md"
+                            readonly>
+                        <div class=" flex  flex-col justify-center w-full gap-2">
+                            <button class="px-4 py-2  gap-2 bg-orange-200 hover:bg-orange-300 rounded-md"
+                                onclick="addEvent()">Add</button>
+                            <button class="px-4 py-2  gap-2 bg-red-400 hover:bg-red-500 rounded-md"
+                                onclick="cancel()">Cancel</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
+    @endif
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
     <script>
         const modal = document.getElementById('modal')
