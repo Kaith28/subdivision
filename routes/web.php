eeb971aff2615584c37875cc0f48b8508ff115e8
Kaith28 <kittykaith4@gmail.com>
2024-04-28 14:39:10 +0800

    add terms and policy

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/terms', function () {
+    return view('terms.terms');
+});
+Route::get('/policy', function () {
+    return view('policy.policy');
+});
 
 /**
  * Profile
